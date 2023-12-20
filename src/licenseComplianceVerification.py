06d9a47b67b09727a4ee2ce9577a47fcf2f79e29
Michele Scarlato <michele.scarlato@gmail.com>
2023-12-20 12:23:57 +0100

    Removed an unused method due to the refactoring (no more calls to LCV API endpoints).

--- src/licenseComplianceVerification.py
+++ src/licenseComplianceVerification.py
@@ -1,34 +1,8 @@
-import time
-import requests
-from lcvlib.verify import Compare_OSADL
-'''
+"""
 * SPDX-FileCopyrightText: 2023 Michele Scarlato
 *
 * SPDX-License-Identifier: Apache-2.0
-'''
-
-
-def license_compliance_verification(inbound_licenses, outbound_license, lcv_url):
-    InboundLicensesString = ';'.join([str(item) for item in inbound_licenses])
-    # LCVComplianceAssessment = LCVurl + "LicensesInput?InboundLicenses=" + InboundLicensesString + "&OutboundLicense=" + outbound_license
-    #LCVComplianceAssessment = (f'{lcv_url}LicensesInput?InboundLicenses={InboundLicensesString}'
-     #                          f'&OutboundLicense={outbound_license}')
-    LCVComplianceAssessment = Compare_OSADL(inbound_licenses, outbound_license)
-    '''
-    try:
-        response = requests.get(url=LCVComplianceAssessment)
-        if response.status_code == 200:
-            LCVComplianceAssessmentResponse = response.json()
-
-    except requests.exceptions.ReadTimeout:
-        print('Connection timeout: ReadTimeout')
-    except requests.exceptions.ConnectTimeout:
-        print('Connection timeout: ConnectTimeout')
-    except requests.exceptions.ConnectionError:
-        print('Connection timeout: ConnectError')
-        time.sleep(30)
-    '''
-    return LCVComplianceAssessment
+"""
 
 
 def generate_inbound_license_set(licenses):
@@ -58,15 +32,11 @@ def parse_lcv_assessment_response(lcv_assessment_response_list, licenses):
                 packageName = licenses[i].get("packageName")
                 packageVersion = licenses[i].get("packageVersion")
                 if licenses[i].get("PyPILicenseSPDX") == InboundNotCompatibleLicense:
-                    # outputPackageInformationNotCompatibleInboundLicensePyPI = "License " + InboundNotCompatibleLicense + \
-                    #    ", declared in PyPI, found in " + packageName + " v. " + packageVersion + "."
                     outputPackageInformationNotCompatibleInboundLicensePyPI = (
                             f'License {InboundNotCompatibleLicense}, declared in PyPI, found in {packageName} '
                             f'v. {packageVersion}.')
                     assessment[j]["packageInformation"] = outputPackageInformationNotCompatibleInboundLicensePyPI
                 if licenses[i].get("GitHubLicense") == InboundNotCompatibleLicense:
-                    outputPackageInformationNotCompatibleInboundLicenseGitHub = "License " + InboundNotCompatibleLicense + \
-                        " declared in GitHub found in " + packageName + " v. " + packageVersion + "."
                     outputPackageInformationNotCompatibleInboundLicenseGitHub = (
                         f'License {InboundNotCompatibleLicense}, declared in GitHub, found in {packageName} '
                         f'v. {packageVersion}.')
@@ -82,7 +52,6 @@ def parse_lcv_assessment_response(lcv_assessment_response_list, licenses):
 
 def parse_license_declared(licenses):
     licenseDeclaredReport = {}
-    i = 0  # assessment index
     for i in licenses:
         licenseDeclaredReport[i] = {}
         if licenses[i].get("PyPILicenseSPDX"):
@@ -96,5 +65,4 @@ def parse_license_declared(licenses):
                                                                 licenses[i].get("packageVersion")
                                                                 + " has been declared with " +
                                                                 licenses[i].get("GitHubLicense") + ", on GitHub API.")
-        i += 1
     return licenseDeclaredReport
