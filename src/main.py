d82fed70524a5e110cfede1d65c0894f0ad7dc7a
Michele Scarlato <michele.scarlato@gmail.com>
2023-12-20 12:48:36 +0100

    Introduced the DependenciesTree class. Removed calls to LCV endpoints.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import argparse
+from createDependenciesTree import DependenciesTree
 from executePypiResolver import ExecutePypiResolver
 from licensesAnalysis import licenses_analysis
 from reportToPDF import ReportToPDF
@@ -10,11 +11,10 @@ def main():
     parser.add_argument("--requirements", type=str, help="Path to the requirements file")
     parser.add_argument("--spdx_license", type=str, help="SPDX id of the license declared for this project")
     args = parser.parse_args()
-
-    lcv_url = 'http://localhost:3251/'
-
+    dependencies_tree = DependenciesTree.create_dependencies_tree(args.requirements, args.product)
+    dependencies_tree_with_licenses = DependenciesTree.increase_dependency_tree_with_license_information(dependencies_tree.name)
     package_list = ExecutePypiResolver.execute_pypi_resolver_for_licensing(args.requirements)
-    licensing_analysis_report = licenses_analysis(args, package_list, lcv_url)
+    licensing_analysis_report = licenses_analysis(args, package_list)
     ReportToPDF.report_to_pdf(licensing_analysis_report, args.product)
 
 
