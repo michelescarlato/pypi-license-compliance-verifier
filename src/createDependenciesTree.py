9c1586ebe9fc6dd98b2020ef7d9e9698bd2b7fa9
Michele Scarlato <michele.scarlato@gmail.com>
2024-01-10 11:19:24 +0100

    Added docstrings for the methods: create_dependencies_tree,increase_dependency_tree_with_license_information,  increase_dependencies_with_licenses.
    Renamed variable f :> dep_tree_json_file.
    
    Introduced recursion to increase with license metadata the dependency tree.

--- src/createDependenciesTree.py
+++ src/createDependenciesTree.py
@@ -3,7 +3,7 @@ import shutil
 import json
 from os.path import join
 from os import getcwd
-from subprocess import run, Popen, PIPE, STDOUT
+from subprocess import PIPE, STDOUT
 from activate_virtualenv import activate_virtualenv
 from retrieveLocallyLicensesInformation import ReceiveLocallyLicensesInformation
 from pypi_lcv_logging import logger
@@ -11,7 +11,15 @@ from pypi_lcv_logging import logger
 
 class DependenciesTree:
     @staticmethod
-    def create_dependencies_tree(requirements_txt, project_name):
+    def create_dependencies_tree(requirements_txt: str, project_name: str):
+        """
+        Create a virtualenv and activate it. Then it installs and runs pipdeptree to create
+        the dependencies tree. It saves pipdeptree output as a json file and return its path.
+
+             Args:
+                 requirements_txt (str): the path containing the Requirements.txt of the analyzed project
+                 project_name (str): the project name set as a command line parameter.
+        """
         venv_dir = join(getcwd(), f'venv-{project_name}')
         result = subprocess.run(['python3', '-m', 'venv', f'venv-{project_name}'],  stdout=PIPE, stderr=STDOUT)
         logger.info(f'python3 -m venv venv-{project_name}')
@@ -28,40 +36,42 @@ class DependenciesTree:
             logger.info(result.stdout)
             subprocess.run([pip3_venv, "install", 'pipdeptree'], cwd=venv_dir, stdout=PIPE, stderr=STDOUT)
             dependencies_tree_json_path = join(getcwd(), project_name)
-            f = open(f"{dependencies_tree_json_path}/dependencies_tree.json", "w")
-            result = subprocess.run(['pipdeptree', '--json-tree'], cwd=venv_dir, stdout=f, stderr=STDOUT)
+            dep_tree_json_file = open(f"{dependencies_tree_json_path}/dependencies_tree.json", "w")
+            result = subprocess.run(['pipdeptree', '--json-tree'], cwd=venv_dir,
+                                    stdout=dep_tree_json_file, stderr=STDOUT)
             logger.info(f'pipdeptree --json-tree')
             logger.info(result.stderr)
         result = subprocess.run(['rm', '-rf', venv_dir], stdout=PIPE, stderr=STDOUT)  # delete the created venv
         logger.info(f'rm -rf {venv_dir}')
         logger.info(result.stdout)
-        return f
+        return dep_tree_json_file
 
     @staticmethod
-    def increase_dependency_tree_with_license_information(dependencies_tree_path):
+    def increase_dependency_tree_with_license_information(dependencies_tree_path: str):
+        """
+        It parses the pipdeptree json file augmenting it with license information.
+        It returns a json augmented with license information.
+
+            Args:
+             dependencies_tree_path (str): the path containing the pipdeptree json file
+        """
         with open(dependencies_tree_path, "r") as jsonFile:
             dep_tree = json.load(jsonFile)
         for dependency in dep_tree:
-            DependenciesTree.collect_dependency_license(dependency)
-            if len(dependency['dependencies']) > 0:
-                for dependency in dependency['dependencies']:
-                    DependenciesTree.collect_dependency_license(dependency)
-                    if len(dependency['dependencies']) > 0:
-                        for dependency in dependency['dependencies']:
-                            DependenciesTree.collect_dependency_license(dependency)
-                            if len(dependency['dependencies']) > 0:
-                                for dependency in dependency['dependencies']:
-                                    DependenciesTree.collect_dependency_license(dependency)
-                                    if len(dependency['dependencies']) > 0:
-                                        for dependency in dependency['dependencies']:
-                                            DependenciesTree.collect_dependency_license(dependency)
+            DependenciesTree.increase_dependencies_with_licenses(dependency)
         dependencies_tree_path = dependencies_tree_path.replace('.json', '')
         with open(f'{dependencies_tree_path}_with_licenses.json', "w") as jsonFile:
             json.dump(dep_tree, jsonFile, indent=4)
-        return
+        return f'{dependencies_tree_path}_with_licenses.json'
 
     @staticmethod
-    def collect_dependency_license(dependency):
+    def increase_dependencies_with_licenses(dependency: dict):
+        """
+        Recursively augment dependencies with license information.
+
+            Args:
+             dependency (dict): a dict containing dependency information
+        """
         logger.info(dependency['package_name'])
         PyPILicense, PyPILicenseSPDX, GitHubLicense, GitHubLicenseSPDX \
             = (ReceiveLocallyLicensesInformation.
@@ -72,3 +82,55 @@ class DependenciesTree:
         dependency['GitHubLicense'] = GitHubLicense
         dependency['GitHubLicenseSPDX'] = GitHubLicenseSPDX
         dependency['dependencies'] = dependency.pop('dependencies')
+        if len(dependency['dependencies']) > 0:
+            for dependency in dependency['dependencies']:
+                DependenciesTree.increase_dependencies_with_licenses(dependency)
+
+    @staticmethod
+    def analyze_dep_tree_with_license_info_recursive(dependencies_tree_with_license_path: str):
+        with open(dependencies_tree_with_license_path, "r") as jsonFile:
+            data = json.load(jsonFile)
+            for dependency in data:
+                DependenciesTree.recurse_over_dependency(dependency)
+
+    @staticmethod
+    def recurse_over_dependency(dict_obj: dict):
+        outbound_license = None
+        inbound_licenses_list = []
+        inbound_and_outbound_data = {'inbound': {}}
+        for key, value in dict_obj.items():
+            if 'dependencies' in key:
+                for item in value:  # loop over the dependencies
+                    DependenciesTree.recurse_over_dependency(item)
+                    license_set, source = DependenciesTree.set_license(item)
+                    inbound_licenses_list.append(license_set)
+                    inbound_and_outbound_data['inbound'][item['package_name']] = \
+                        {"package_name": item['package_name'],
+                         "SPDX_license": license_set, "license_origin": source}
+            if 'package_name' in key:
+                outbound_license, source = DependenciesTree.set_license(dict_obj)
+                inbound_and_outbound_data['outbound'] = \
+                    {"package_name": dict_obj['package_name'],
+                     "SPDX_license": outbound_license, "license_origin": source}
+                continue
+
+            if len(inbound_licenses_list) > 0 and outbound_license is not None:
+                logger.info(f'In this iteration: inbound licenses: {inbound_licenses_list} - '
+                            f'and outbound license: {outbound_license} - found')
+
+                logger.info(inbound_and_outbound_data)
+
+    @staticmethod
+    def set_license(item: dict):
+        license_set = ""
+        source = ""
+        if item['PyPILicenseSPDX'] is not None and item['PyPILicenseSPDX'] != "":
+            license_set = item['PyPILicenseSPDX']
+            source = 'PyPI API'
+            logger.info(f'Package: {item["package_name"]} - PyPI license: {item["PyPILicenseSPDX"]}')
+        else:
+            if item['GitHubLicenseSPDX'] is not None and item['GitHubLicenseSPDX'] != "":
+                license_set = item['GitHubLicenseSPDX']
+                source = 'GitHub API'
+                logger.info(f'Package: {item["package_name"]} - GitHub license: {item["GitHubLicenseSPDX"]}')
+        return license_set, source
